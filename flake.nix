818a3164a63f5c7c88fadf23534bb36a1e75247a
Zebreus <zebreus@zebre.us>
2024-09-11 13:16:41 +0200

    Improve development environment

--- flake.nix
+++ flake.nix
@@ -29,16 +29,51 @@
             pkgs.steam-run
           ];
           shellHook = ''
-            go install github.com/toitlang/jaguar/cmd/jag@latest 
+            function wrapProgram() {
+              local directory="$(dirname $1)"
+              local file="$(basename $1)"
+              mv $directory/$file $directory/.unwrapped_$file
+              cat <<EOF > $directory/$file
+            #!/usr/bin/env bash
+            steam-run $directory/.unwrapped_$file "\$@"
+            EOF
+            chmod a+x $directory/$file
+            }
+
+            # Add a directory for binaries that will be linked into path
             mkdir -p ~/.cache/hackyJaguarFlake/bin
+            export PATH=~/.cache/hackyJaguarFlake/bin:$PATH
+
+            # Install the latest jag via go into the user home
+            go install github.com/toitlang/jaguar/cmd/jag@latest 
+
+            # Create a steam-run wrapper in our bin directory
             cat <<EOF > ~/.cache/hackyJaguarFlake/bin/jag
             #!/usr/bin/env bash
             steam-run $HOME/go/bin/jag "\$@"
             EOF
             chmod a+x ~/.cache/hackyJaguarFlake/bin/jag
-            export PATH=~/.cache/hackyJaguarFlake/bin:$PATH
-            sudo nft add rule inet filter input-allow udp dport 1990 accept
-            # export PATH=~/go/bin/:$PATH
+
+            # Download toit and jaguar tools
+            # And wrap them in steam-run
+            if ! jag setup --check ; then
+              jag setup
+              find $HOME/.cache/jaguar/sdk -type f -executable | while read line ; do
+                wrapProgram $line
+              done
+            fi
+
+            # Link jaguar tools into temporary bin
+            find $HOME/.cache/jaguar/sdk/bin $HOME/.cache/jaguar/sdk/tools -type f -executable | while read line ; do
+              ln -sf $line ~/.cache/hackyJaguarFlake/bin/$(basename $line)
+            done
+
+            # Open udp port 1990 for finding jaguar devices
+            if which nft ; then
+              sudo nft add rule inet nixos-fw input-allow udp dport 1990 accept
+            else
+              echo Make sure that UDP port 1990 is open, otherwise scanning for esp devices wont work
+            fi
           '';
         };
 
